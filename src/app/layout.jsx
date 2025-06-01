2ea13faf5befb1c86c7c04a6377cde093dfab40c
JJyen <gdb1632@naver.com>
2025-06-02 00:04:30 +0900

    feat: 네비게이션바 UI 추가

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,40 @@
 import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
+  );
+}
+
+function NavBar() {
+  return (
+    <div className="nav-container">
+      <div className="logo-box">
+        <img src="/assets/logo.png" alt="로고" />
+      </div>
+      <div className="nav-box">
+        <Link href="/#" className="nav-item">
+          홈
+        </Link>
+        <Link href="/#" className="nav-item">
+          기록하기
+        </Link>
+        <Link href="/#" className="nav-item">
+          이달의정산
+        </Link>
+        <Link href="/#" className="nav-item">
+          내정보
+        </Link>
+      </div>
+      <div className="signup-btn">
+        <Link href="/#">회원가입</Link>
+      </div>
+    </div>
   );
 }
