ccb16f5d0ef45da82b6922010a0539b2443b9308
JJyen <gdb1632@naver.com>
2025-06-18 22:44:51 +0900

    feat: 기록 페이지 컨텐츠 배치

--- src/app/record/page.jsx
+++ src/app/record/page.jsx
@@ -1,7 +1,25 @@
+"use client";
+
+import "./record.css";
+import Dropdown from "@/componets/dropdown/Dropdown";
+
 export default function Record() {
+  // const currentPath = usePathname();
+  // console.log(currentPath ? currentPath : "null");
+
   return (
-    <div>
-      <h1>기록하기</h1>
+    <div className="record-container">
+      <header className="record-header">
+        <div className="search-box">
+          <Dropdown name={"연도"} />
+          <Dropdown name={"월"} />
+          <Dropdown name={"일"} />
+          <Dropdown name={"분류"} />
+          <button className="search-btn">찾기</button>
+        </div>
+        <button className="add-record">기록 추가하기</button>
+      </header>
+      <main className="aa"></main>
     </div>
   );
 }
